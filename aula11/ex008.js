dee0f2ee791e96bdfe7eeb8ececc5a10f263b960
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-05-05 11:25:01 -0300

    ex011 update

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log (`a velocidade do seu carro é ${vel} km/h`)
+
+if (vel >60) { //condição simples  
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`diriga sempre usando cinto de segurança`)

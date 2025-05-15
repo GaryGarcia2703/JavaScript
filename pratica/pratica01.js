6a23cd2cb0a3059b394a91f474b6228c66dcfccd
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-05-15 11:16:52 -0300

    pratica

---
+++ pratica/pratica01.js
@@ -0,0 +1,11 @@
+//teste para aprova o aluno
+function calcular () {
+    var n1 = document.querySelector('input#n1').value
+    var n2 = document.querySelector('input#n2').value
+    var button = document.querySelector('button#button')
+    media = n1 + n2
+
+    if (media => 6) {
+        
+    }
+}

4bd33a264eea98dbf02be85f5188ade09b8f4a6f
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-05-15 10:51:43 -0300

    hora do dia
    hora do dia update

--- aula13/scripto.js
+++ aula13/scripto.js
@@ -1,1 +1,25 @@
-alert('olá lil bro')
+function carregar () {
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 22  // para teste
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'imagens/dia.jpg'
+        document.body.style.background = '#44A1F2'
+
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        img.src = 'imagens/tarde_sonicp.png'
+        document.body.style.background = '#b42e10d8'
+
+    } else {
+        //boa noite
+        img.src = 'imagens/noite1.jpg'
+        document.body.style.background = '#0F5FA6'
+    }
+}

474296521f9936df66c693bc968b788361fb8c9c
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-05-20 11:23:00 -0300

    ex14 update

--- aula14ex/scripto.js
+++ aula14ex/scripto.js
@@ -1,1 +1,16 @@
-alert('olá lil bro')
+function verificar () {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('input#txtano')
+    var res = document.querySelector('div#res')
+
+        if (fano.value.length == 0 || Number(fano.value) > ano) {
+                window.alert('[ERRO] dados invalidos')
+        } else {
+            var fsex = document.getElementsByName('radsex')
+            var idade = ano - Number(fano.value)
+            res.innerHTML = `idade calculada: ${idade}`
+        }
+}
+var button = document.querySelector('input#verificar')
+button.addEventListener('click' , verificar)

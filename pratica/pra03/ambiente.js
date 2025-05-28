9acb3d5680409b2ee2bc61737310861898d08c39
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-05-28 11:12:23 -0300

    Create ambiente.js

---
+++ pratica/pra03/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++ // c = c +1
+} while (c <= 8)
+
+
+
+
+/*
+console.log('olá lil blud')
+console.log('olá lil blud')
+console.log('olá lil blud')
+console.log('olá lil blud')
+console.log('olá lil blud')
+console.log('olá lil blud')
+console.log('olá lil blud')
+*/

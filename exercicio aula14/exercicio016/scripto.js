05234d54a2ac0571af25e4a02aea7ddcc3b8776c
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-06-05 10:53:32 -0300

    updateeee

--- exercicio aula14/exercicio016/scripto.js
+++ exercicio aula14/exercicio016/scripto.js
@@ -1,1 +1,12 @@
-alert('começando a codar lil bro')
+var btn = document.querySelector('button#btn')
+let ini = Number(document.querySelector('input#ini'))
+let fim = Number(document.querySelector('Input#fom'))
+let passo = Number(document.querySelector('input#passo'))
+
+function contar() {
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        window.alert('tudo ok')
+    }
+
+
+}

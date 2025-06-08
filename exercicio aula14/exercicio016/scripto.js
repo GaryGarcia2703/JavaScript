1072a9491786e084780c5126048607d6c1262e1b
Gary García <Gary.Garcia2703@gmail.com>
2025-06-08 14:59:54 -0300

    Update scripto.js

--- exercicio aula14/exercicio016/scripto.js
+++ exercicio aula14/exercicio016/scripto.js
@@ -5,13 +5,15 @@ function contar() {
     let passoInput = document.querySelector('input#passo');
 
     if (iniInput.value.length === 0 || fimInput.value.length === 0 || passoInput.value.length === 0) {
+
         window.alert('[ERRO] Dados não informados');
+
     } else {
         let ini = Number(iniInput.value);
         let fim = Number(fimInput.value);
         let passo = Number(passoInput.value);
-        alert('Tudo ok');
-        // Aqui você pode colocar o código para fazer a contagem
+
+        for (let c = 1) 
     }
 
 }

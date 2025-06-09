d5a944a9c75a1888d98f84f3a2d524ef7c3b9dfb
Gary García <Gary.Garcia2703@gmail.com>
2025-06-09 11:48:29 -0300

    Update scripto.js

--- exercicio aula14/exercicio016/scripto.js
+++ exercicio aula14/exercicio016/scripto.js
@@ -1,25 +1,40 @@
 function contar() {
-
     let iniInput = document.querySelector('input#ini');
     let fimInput = document.querySelector('input#fim');
     let passoInput = document.querySelector('input#passo');
-    let res = document.querySelector('div#res')
+    let res = document.querySelector('div#res');
 
     if (iniInput.value.length === 0 || fimInput.value.length === 0 || passoInput.value.length === 0) {
-
+        
         window.alert('[ERRO] Dados não informados');
 
     } else {
+        res.innerHTML = ''; 
+
         let ini = Number(iniInput.value);
         let fim = Number(fimInput.value);
         let passo = Number(passoInput.value);
 
-        for (let c = ini; c <= fim; c += 1 passo) {
-            res.innerHTML += `${c}`
-        } 
-    }
+        if (passo <= 0) {
+            window.alert('[ERRO] Passo inválido! Considerando PASSO 1');
+            passo = 1;
+        }
 
+        if (ini < fim) {
+            // Contagem crescente
+            for (let c = ini; c <= fim; c += passo) {
+                res.innerHTML += `${c} 👉 `;
+            }
+        } else {
+            // Contagem decrescente
+            for (let c = ini; c >= fim; c -= passo) {
+                res.innerHTML += `${c} 👉 `;
+            }
+        }
+
+        res.innerHTML += `🏁`;
+    }
 }
-    
-var btn = document.querySelector('button#btn');
-btn.addEventListener('click' , contar)
+
+let btn = document.querySelector('button#btn');
+btn.addEventListener('click', contar);

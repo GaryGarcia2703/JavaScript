a95ee2912f2db9b557ccd39a3ad61d6b7e91ee8a
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-06-11 10:21:58 -0300

    tabuada.update

--- exercicio aula14/exercicio017/scripto.js
+++ exercicio aula14/exercicio017/scripto.js
@@ -1,1 +1,20 @@
-alert('olá lil bro')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+
+    if (num.value.length == 0) {
+        alert('por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        
+        for (let contador = 1; contador <=10 ; contador++) {
+             
+            let item = document.createElement('option');
+            item.text = `${n} x ${contador} = ${n*contador}`;
+            item.value = `tab${contador}`; 
+            tab.appendChild(item);
+        }
+    }
+}
+

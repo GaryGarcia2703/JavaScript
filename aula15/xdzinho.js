4398ced6525b790ef840ba7d1882b00b0f481deb
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-06-24 12:49:31 -0300

    Create xdzinho.js

---
+++ aula15/xdzinho.js
@@ -0,0 +1,9 @@
+let num = [1,2,3,4,5,6,8,9,0]
+num.push(12)
+num.sort((a, b) => a - b);
+
+for (let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+console.log(num)

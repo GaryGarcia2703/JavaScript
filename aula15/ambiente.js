c5675eee7ed8a8cfd9571a9b5bf813ce3ffb2f0e
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-06-29 15:46:05 -0300

    location

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,7 +4,7 @@ num.sort((a, b) => a - b);
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(30)
+let pos = num.indexOf(6)
 console.log(`o valor 8 esta na posição ${pos}`)
  if (pos == -1) {
     console.log('o valor não foi encontrado')

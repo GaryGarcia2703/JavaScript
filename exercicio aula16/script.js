2f4386b95bb9c07316e5a7eff064e2d746531f29
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-07-15 11:33:49 -0300

    update

--- exercicio aula16/script.js
+++ exercicio aula16/script.js
@@ -1,44 +1,19 @@
 let num = document.querySelector('input#numero')
 let lista = document.querySelector('select#lista')
 let res = document.querySelector('div#res')
+let addbtn = document.querySelector('button#add')
 let valores = []
 
-function isNumero(n) {
-    if ((Number(n)) >= 1 && (Number(n)) <= 100) {
-       return true
-    } else {
-        return false
-    }
-}
-
-function inLista(n , l) {
-    if (lista.indexOf(Number(n)) != -1) {
-       return true
-    } else {
-        return false
-    }
-}
-
 function adicionar() {
-    // Verifica se é um número válido E se NÃO está na lista
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        // 1. Adiciona o número ao array 'valores'
-        valores.push(Number(num.value));
-
-        // 2. Cria um novo item (option) para o <select>
-        let item = document.createElement('option');
-        item.text = `Valor ${num.value} adicionado.`;
-        lista.appendChild(item); // Adiciona o item à lista (select)
-
-        // 3. Limpa a área de resultado (se houver alguma mensagem anterior)
-        res.innerHTML = ''; 
+    if (num.ariaValueMax.length === 0) {
+        alert('Informe um valor para ser inderido porfavor')
 
     } else {
-        // Se o valor for inválido ou já estiver na lista
-        window.alert('Valor inválido ou já encontrado na lista!');
-    }
+        let num = Number(num.value)
+        valores.push(num)
 
-    // Limpa o campo de entrada e foca nele para nova entrada
-    num.value = '';
-    num.focus();
+        num.value = '';
+
+    }
 }
+addbtn.addEventListener('click' , adicionar)

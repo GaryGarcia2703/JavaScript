b1dd229b4327658c8458add409ce95cc3790127e
GaryGarcia2703 <Gary.Garcia2703@gmail.com>
2025-07-24 22:11:58 -0300

    update

--- exercicio aula16/script.js
+++ exercicio aula16/script.js
@@ -1,19 +1,41 @@
-let num = document.querySelector('input#numero')
-let lista = document.querySelector('select#lista')
-let res = document.querySelector('div#res')
-let addbtn = document.querySelector('button#add')
 let valores = []
+let valor = document.querySelector('input#numero')
+let addbtn = document.querySelector('button#add')
+let finbtn = document.querys
+let res = document.querySelector('div#res')
+let lista = document.querySelector('select#lista')
 
-function adicionar() {
-    if (num.ariaValueMax.length === 0) {
-        alert('Informe um valor para ser inderido porfavor')
-
+function add() {
+    if (valor.value.length === 0) {
+        window.alert('Insira um número [a caixa está vazia]')
     } else {
-        let num = Number(num.value)
-        valores.push(num)
+        let num = Number(valor.value)
 
-        num.value = '';
+        if (num < 1 || num > 100) {
+            window.alert('Insira um valor [entre 1 e 100]')
+            valor.value = '' 
+            valor.focus() 
+            
+        } else {
 
+        if (valores.indexOf(num) != -1) {
+            window.alert('Valor já informado lil bro')
+            valor.value= '';
+            valor.focus()
+            return
+        }
+            valores.push(num)
+            window.alert(`Número ${num} adicionado com sucesso!`) 
+            console.log(valores)
+
+            let item = document.createElement('option') // crea el elemento
+            item.text = `Valor ${num} adicionado`       
+            lista.appendChild(item) //muestra el elemento creado
+            valor.value = '' 
+            valor.focus() 
+        }
+
+   
     }
 }
-addbtn.addEventListener('click' , adicionar)
+addbtn.addEventListener('click', add)
